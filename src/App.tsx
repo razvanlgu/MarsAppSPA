908975cf66870af6a13c373eb043050980613a0f
Razvan Lungu <Razvan.Lungu@softwire.com>
2020-07-02 16:04:21 +0300

    fully added context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, {Dispatch, SetStateAction, useContext, useState} from 'react';
-import logo from './nasa-logo.png';
 import './App.css';
 
-const MyContext = React.createContext({
-    data: 0,
-    setData: (data: number) => {data = 0}
-});
+interface MyContextData {
+    data: number;
+    setData: (data: number) => void;
+}
 
+const MyContext = React.createContext<MyContextData>({} as MyContextData);
 
 function App() {
     const nasaArticle: ArticleWithPhotoData = {
@@ -15,21 +15,15 @@ function App() {
         paragraph2: "NASA was established in 1958, succeeding the National Advisory Committee for Aeronautics (NACA). The new agency was to have a distinctly civilian orientation, encouraging peaceful applications in space science.[8][9][10] Since its establishment, most US space exploration efforts have been led by NASA, including the Apollo Moon landing missions, the Skylab space station, and later the Space Shuttle. NASA is supporting the International Space Station and is overseeing the development of the Orion spacecraft, the Space Launch System, and Commercial Crew vehicles. The agency is also responsible for the Launch Services Program, which provides oversight of launch operations and countdown management for uncrewed NASA launches.",
         imgSrc: "./nasa-logo.png"
     }
-    const ls = localStorage;
-    ls.setItem("count", "0");
 
     const [data, setData] = useState(0);
 
   return (
       <MyContext.Provider value={{data, setData}}>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-            <body>
-                <ButtonCounter/>
-                <ArticleWithPhoto title={nasaArticle.title} paragraph1={nasaArticle.paragraph1} paragraph2={nasaArticle.paragraph2} imgSrc={nasaArticle.imgSrc}/>
-            </body>
+            <ButtonCounter/>
+            <TextAndNumber text="Here is how many times you pressed the button."/>
+            <ArticleWithPhoto {...nasaArticle}/>
         </div>
       </MyContext.Provider>
   );
@@ -54,23 +48,40 @@ const ArticleWithPhoto: React.FC<ArticleWithPhotoData> = (props) => {
 }
 
 
-const ButtonCounter: React.FC = () => {
-    // const clickCountString = localStorage.getItem("count")
-    // let clickCount: number;
-    // if (clickCountString == null) {
-    //     clickCount = 0;
-    // } else {
-    //     clickCount = parseInt(clickCountString)
-    // }
-    // const [noClicks, setNoClicks] = useState(clickCount);
+interface Text {
+    text: string;
+}
 
+const TextAndNumber: React.FC<Text> = (props) => {
+    return (
+        <div>
+            <MyTextBox text={props.text}/>
+            <MyNumber/>
+        </div>
+    );
+}
+
+const ButtonCounter: React.FC = () => {
     const context = useContext(MyContext);
 
     return (
         <div>
             <button onClick={() => context.setData(context.data + 1)}> Click me!!! </button>
-            <p> The button was clicked {context.data} times! </p>
         </div>
+    );
+}
+
+const MyTextBox: React.FC<Text> = (props) => {
+    return (
+        <p>{props.text}</p>
+    );
+}
+
+const MyNumber: React.FC = () => {
+    const context = useContext(MyContext);
+
+    return (
+      <div>{context.data}</div>
     );
 }
 
